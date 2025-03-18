55513aa82b3a07916252970f88beedcd34b0b3d8
wploits!!! <166282207+wploits@users.noreply.github.com>
2025-03-18 10:36:10 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ elseif placeid == 13772394625 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/wploits/critclhub/refs/heads/main/bladeball.lua'))()
 elseif placeid == 18668065416 or placeid == 115110570222234 then
   print("loading bluelock...")
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/wploits/critclhub/refs/heads/main/bluelockrivlas.lua'))()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/wploits/critclhub/refs/heads/main/bluelockrivals.lua'))()
 else
   warn("unsupported game")
   loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
