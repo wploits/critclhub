f00716b4eba40f158d5b1000ab868512b016e99b
wploits!!! <166282207+wploits@users.noreply.github.com>
2025-03-18 10:08:49 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@ local placeid = game.PlaceId
 if placeid == 6961824067 then
   print("loading ftap...")
   loadstring(game:HttpGet('https://raw.githubusercontent.com/wploits/critclhub/refs/heads/main/ftap.lua'))()
+elseif placeid == 13772394625 then
+  print("loading bladeball...")
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/wploits/critclhub/refs/heads/main/bladeball.lua'))()
 else
   warn("unsupported game")
   loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
